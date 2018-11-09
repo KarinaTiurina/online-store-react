b20403d34a816b7640571ac05cd9905de1912cc2
KarinaTiurina <rktiurina@gmail.com>
2018-11-09 20:56:58 +0300

    add constants/products

---
+++ constants/products.js
@@ -0,0 +1,40 @@
+const products = () => ([
+  {
+    id: 1,
+    title: 'Item1',
+    price: 12.3,
+    imageUrl: ''
+  },
+  {
+    id: 2,
+    title: 'Item2',
+    price: 15.3,
+    imageUrl: ''
+  },
+  {
+    id: 3,
+    title: 'Item3',
+    price: 32.3,
+    imageUrl: ''
+  },
+  {
+    id: 4,
+    title: 'Item4',
+    price: 12.4,
+    imageUrl: ''
+  },
+  {
+    id: 5,
+    title: 'Item5',
+    price: 15.3,
+    imageUrl: ''
+  },
+  {
+    id: 6,
+    title: 'Item6',
+    price: 62.3,
+    imageUrl: ''
+  }
+]);
+
+export default products;
