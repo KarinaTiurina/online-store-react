92aa9359e6bd0df2b17f201c6e41563e2faf6ade
KarinaTiurina <rktiurina@gmail.com>
2018-11-25 20:31:40 +0300

    update view Navigation

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from '~/src/components/views/Navigation';
 import Basket from '~/src/components/containers/Basket';
 import productsContext from '~/context/ProductsContext';
 import products from '~/constants/products';
+import routes from '~/src/routes';
+
+const RouteWithSubroutes = (route, key) => (
+  <Route key={key} {...route} />
+);
 
 class App extends Component {
   render() {
     return (
       <productsContext.Provider value={products}>
         <Basket>
-          <Navigation />
+          <Router>
+            <div>
+              <Navigation />
+              <Switch>
+                {routes.map((route, i) => RouteWithSubroutes(route, i))}
+              </Switch>
+            </div>
+          </Router>
         </Basket>
       </productsContext.Provider>
     );
